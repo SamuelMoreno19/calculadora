9d059dfc6b83d7f21ac804632609e7f103ef61d8
Samuel Moreno <morenosamuel2007@gmail.com>
2025-08-29 16:55:47 -0500

    Funcion para el calculo factorial

--- index.js
+++ index.js
@@ -1,49 +1,49 @@
 // Función para sumar dos números
 function sumar(num1, num2) {
-  return num1 + num2;
+    return num1 + num2;
 }
 
 // Función principal del programa
 function calculadora() {
-  var opcion = prompt(
-    "Elija una opción:\n1. Suma\n2. Resta\n3. Multiplicación\n4. División\n5. Factorial\n6. Exponente\n7. Raíz cuadrada"
-  );
-  var num1 = parseFloat(prompt("Ingrese el primer número:"));
-  var num2, resultado;
+    var opcion = prompt(
+        "Elija una opción:\n1. Suma\n2. Resta\n3. Multiplicación\n4. División\n5. Factorial\n6. Exponente\n7. Raíz cuadrada"
+    );
+    var num1 = parseFloat(prompt("Ingrese el primer número:"));
+    var num2, resultado;
 
-  if (opcion !== "5" && opcion !== "7") {
-    num2 = parseFloat(prompt("Ingrese el segundo número:"));
-  }
+    if (opcion !== "5" && opcion !== "7") {
+        num2 = parseFloat(prompt("Ingrese el segundo número:"));
+    }
 
-  switch (opcion) {
-    case "1":
-      resultado = sumar(num1, num2);
-      break;
-    case "2":
-      resultado = restar(num1, num2);
-      break;
-    case "3":
-      resultado = multiplicar(num1, num2);
-      break;
-    case "4":
-      resultado = dividir(num1, num2);
-      break;
-    case "5":
-      resultado = factorial(num1);
-      break;
-    case "6":
-      var exponenteNum = parseFloat(prompt("Ingrese el exponente:"));
-      resultado = exponente(num1, exponenteNum);
-      break;
-    case "7":
-      resultado = raizCuadrada(num1);
-      break;
-    default:
-      resultado = "Opción no válida";
-      break;
-  }
+    switch (opcion) {
+        case "1":
+            resultado = sumar(num1, num2);
+            break;
+        case "2":
+            resultado = restar(num1, num2);
+            break;
+        case "3":
+            resultado = multiplicar(num1, num2);
+            break;
+        case "4":
+            resultado = dividir(num1, num2);
+            break;
+        case "5":
+            resultado = factorial(num1);
+            break;
+        case "6":
+            var exponenteNum = parseFloat(prompt("Ingrese el exponente:"));
+            resultado = exponente(num1, exponenteNum);
+            break;
+        case "7":
+            resultado = raizCuadrada(num1);
+            break;
+        default:
+            resultado = "Opción no válida";
+            break;
+    }
 
-  alert("El resultado es: " + resultado);
+    alert("El resultado es: " + resultado);
 }
 
 // Llamar a la función principal
@@ -66,4 +66,14 @@ function dividir(num1, num2) {
 }
 
 //Funcion para calculo factorial
+function factorial(num) {
+    if (num < 0) {
+        return "No existe factorial para números negativos";
+    }
+    let resultado = 1;
+    for (let i = 1; i <= num; i++) {
+        resultado *= i;
+    }
+    return resultado;
+}
 
